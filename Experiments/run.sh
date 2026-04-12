d397d516d78826aee27ec44d8e8dd457cd7bf731
magn3144 <magnus@elgaard.com>
2026-04-12 20:11:47 +0200

    run.sh

---
+++ Experiments/run.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+### General options
+### –- specify queue --
+#BSUB -q gpuv100
+### -- set the job Name --
+#BSUB -J calculate_geodesics
+### -- ask for number of cores (default: 1) --
+#BSUB -n 4
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
+#BSUB -W 12:00
+# request system-memory
+#BSUB -R "rusage[mem=8GB]"
+### -- set the email address --
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+##BSUB -u s204164@dtu.dk
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o batch_output/geodesics_%J.out
+#BSUB -e batch_output/geodesics_%J.err
+# -- end of LSF options --
+
+nvidia-smi
+# Load the cuda module
+module load cuda/11.6
+
+/appl/cuda/11.6.0/samples/bin/x86_64/linux/release/deviceQuery
+
+source ../.venv/bin/activate
+python ensemble_vae.py
